68cfcf79f493b8d82cf3ac186a5697ca9cd2249e
mlunchar <mlunchar@gmail.com>
2018-05-11 06:22:40 +0300

    Add ExistantActivity class and existant_activity layout

---
+++ app/src/main/java/shecodeshack/org/audioview/activity/ExistantActivity.java
@@ -0,0 +1,22 @@
+package shecodeshack.org.audioview.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import shecodeshack.org.audioview.R;
+
+public class ExistantActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.existant_activity);
+    }
+
+
+
+
+
+}
